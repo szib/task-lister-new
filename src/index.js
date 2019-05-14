e83f09caa18c829aa70f8b5ae6c5ab8ea93f8287
Ivan Szebenszki <szib77@gmail.com>
2019-05-14 16:01:21 +0100

    Sort tasks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   const tasksEl = document.querySelector('#tasks');
   const formEl = document.querySelector('#create-task-form');
+  let taskData = [];
+
+  function byPriority(a, b) {
+    return b[1] - a[1];
+  }
+
+  function byName(a, b) {
+    if (a > b) {
+      return 1;
+    } if (a < b) {
+      return -1;
+    }
+    return 0;
+  }
+
+  let sorterFunction = byName;
 
   function color(index) {
     switch (index) {
@@ -14,12 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function addTask(task) {
-    const listEl = document.createElement('li');
     const priority = document.querySelector('#task-priority');
+    taskData = [...taskData, [task, priority.options.selectedIndex]];
+  }
 
-    listEl.setAttribute('data-priority', priority.options.selectedIndex);
-    listEl.style.color = color(priority.options.selectedIndex);
-
+  function buildLi(task, priority) {
+    const listEl = document.createElement('li');
+    listEl.setAttribute('data-priority', priority);
+    listEl.style.color = color(priority);
     listEl.innerHTML = `${task}<button>X</button>`;
     tasksEl.appendChild(listEl);
 
@@ -27,12 +45,31 @@ document.addEventListener('DOMContentLoaded', () => {
     deleteBtn.addEventListener('click', () => {
       listEl.remove();
     });
+    return listEl;
+  }
+
+  function buildUl() {
+    tasksEl.innerHTML = '';
+    taskData.sort(sorterFunction).forEach((data) => {
+      tasksEl.appendChild(buildLi(data[0], data[1]));
+    });
   }
 
   formEl.addEventListener('submit', (event) => {
     event.preventDefault();
     const task = formEl['new-task-description'].value;
     addTask(task);
+    buildUl();
     formEl.reset();
+  });
+
+  document.querySelector('#btn-by-name').addEventListener('click', () => {
+    sorterFunction = byName;
+    buildUl();
+  });
+
+  document.querySelector('#btn-by-priority').addEventListener('click', () => {
+    sorterFunction = byPriority;
+    buildUl();
   });
 });
