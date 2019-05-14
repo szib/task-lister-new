1b3484885e0618504d1720934b695561d1df58d3
Ivan Szebenszki <szib77@gmail.com>
2019-05-14 14:22:53 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener('DOMContentLoaded', () => {
+  const tasksEl = document.querySelector('#tasks');
+  const formEl = document.querySelector('#create-task-form');
+
+  function addTask(task) {
+    const listEl = document.createElement('li');
+    listEl.textContent = task;
+    tasksEl.appendChild(listEl);
+  }
+
+  formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const task = formEl['new-task-description'].value;
+    addTask(task);
+    formEl.reset();
+  });
 });
