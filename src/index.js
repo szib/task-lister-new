1c69ac1a89a61f8d6595b915e27a76baa1b44659
Ivan Szebenszki <szib77@gmail.com>
2019-05-14 14:45:53 +0100

    Priority

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const tasksEl = document.querySelector('#tasks');
   const formEl = document.querySelector('#create-task-form');
 
+  function color(index) {
+    switch (index) {
+      case 0:
+        return 'green';
+      case 2:
+        return 'red';
+      default:
+        return 'yellow';
+    }
+  }
+
   function addTask(task) {
     const listEl = document.createElement('li');
+    const priority = document.querySelector('#task-priority');
+    listEl.style.color = color(priority.options.selectedIndex);
     listEl.innerHTML = `${task}<button>X</button>`;
     tasksEl.appendChild(listEl);
     const deleteBtn = listEl.querySelector('button');
